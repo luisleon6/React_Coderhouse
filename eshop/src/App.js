469a145d21dbfb9bd6e1d3cce1011302c0bb831d
FarahHadid <j.luisleon6@gmail.com>
2022-06-08 23:39:52 -0500

    Navbar created with react

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Services from './pages/services';
+import Contact from './pages/contact-us';
+import SignUp from './pages/sign-up';
+import SignIn from './pages/signin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/contact-us' element={<Contact/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/signin' element={<SignIn/>} />
+      </Routes>
+    </Router>
   );
 }
 
